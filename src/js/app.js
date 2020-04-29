80b0ccb4110044d4cbd14e9a35b8c08757a1a865
Bulat <sadikovba@gmail.com>
2020-04-29 14:58:23 +0300

    Initial

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+//import api from "./services/apiService";
+//
+//api.countries().then((res) => console.log(res));
+//api.cities().then((res) => console.log(res));
+
+import "../css/style.css";
+import "./plugins";
+import locations from "./store/locations";
+
+locations.init().then((res) => {
+  console.log(res);
+  console.log(locations);
+
+  console.log(locations.getCitiesByCountryCode("PE"));
+});
